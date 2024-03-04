d64ea777d13abc666da72ad0b5f549f5296301f5
Clayton Fernalo <clafernalo@gmail.com>
2024-03-04 19:42:47 +0800

    add threading to video downloading

--- download_video_script.py
+++ download_video_script.py
@@ -1,5 +1,11 @@
 import json
+import queue
+import threading
 from pytube import YouTube
+
+
+num_of_threads = 16
+
 def download_videos(video_id, video_info):
     url = video_info['url']
     file_name = f"{video_id}.mp4"
@@ -8,8 +14,23 @@ def download_videos(video_id, video_info):
     yt.streams.get_highest_resolution().download(output_path='./download_videos', filename=file_name)
     print(f"Downloaded video: {file_name}")
 
+
+def worker(q):
+    while True:
+        video_id, video_info = q.get()
+        download_videos(video_id, video_info)
+        q.task_done()
+
 if __name__ == "__main__":
-    with open('./metafiles/hdvg_0_first_10.json', 'r') as f:
+    q = queue.Queue()
+    with open('./metafiles/hdvg_0.json', 'r') as f:
         video_info = json.load(f)
     for video_id in video_info:
-        download_videos(video_id, video_info[video_id])
+        q.put((video_id, video_info[video_id]))
+
+    for i in range(num_of_threads):
+        threading.Thread(target=worker, daemon=False, args=(q,)).start()
+    
+    q.join()
+    
+    
