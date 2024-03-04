e780a4c15807d05c8b38411d03b49637982afd1c
JvThunder <joshuaadrianc@gmail.com>
2024-03-04 18:51:12 +0800

    edit download script

--- download_video_script.py
+++ download_video_script.py
@@ -7,3 +7,9 @@ def download_videos(video_id, video_info):
     yt = YouTube(url)
     yt.streams.get_highest_resolution().download(output_path='./download_videos', filename=file_name)
     print(f"Downloaded video: {file_name}")
+
+if __name__ == "__main__":
+    with open('./metafiles/hdvg_0_first_10.json', 'r') as f:
+        video_info = json.load(f)
+    for video_id in video_info:
+        download_videos(video_id, video_info[video_id])
