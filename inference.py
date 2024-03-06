4ee0fb86e20e62ee69d8809b4c35c0290fe3873f
JvThunder <joshuaadrianc@gmail.com>
2024-03-07 02:52:28 +0800

    fix: filtering

--- inference.py
+++ inference.py
@@ -1,3 +1,9 @@
+from utils.filter_scenes import filter_scenes
+from utils.optical_flow_check import get_optical_flow
+from utils.static_check import get_static_difference
+from utils.image_to_embedding import get_image_to_embedding
+from utils.dataset_class_batch import VideoDataset
+from diffusers import AutoencoderKL
 import json
 import os
 import numpy as np
@@ -6,29 +12,25 @@ import torch
 from torchvision import transforms
 import warnings
 import time
+import pickle
 warnings.filterwarnings("ignore")
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-from diffusers import AutoencoderKL
-from utils.dataset_class_batch import VideoDataset
-from utils.image_to_embedding import get_image_to_embedding
-from utils.static_check import get_static_difference
-from utils.optical_flow_check import get_optical_flow
-from utils.filter_scenes import filter_scenes
 
 def image_transform(image):
     transform = transforms.Compose([
         transforms.Resize((320, 240)),
         transforms.CenterCrop(240),
-        transforms.ToTensor(),  
+        transforms.ToTensor(),
     ])
     return transform(image).unsqueeze(0)
+
 
 def load_image(folder_path):
     # Sort the frames
     frames = sorted(os.listdir(folder_path))
     conv_frames = []
-    
+
     # Convert the frames to tensor
     for frame in frames:
         image = Image.open(os.path.join(folder_path, frame)).convert('RGB')
@@ -36,9 +38,11 @@ def load_image(folder_path):
         conv_frames.append(image)
     return conv_frames
 
+
 def get_model():
     model = AutoencoderKL.from_pretrained("stabilityai/sdxl-vae")
     return model
+
 
 def get_metrics(dataset, model, device):
     res = {}
@@ -46,79 +50,84 @@ def get_metrics(dataset, model, device):
         starttime = time.time()
 
         scene_id = data['scene_id']
-        print(f'Processing scene id: {scene_id}')
         frames_path = data['frames_path']
         frames = load_image(frames_path)
-        
+
         # Getting static difference
-        rgb_diff = get_static_difference(frames)
+        max_rgb_diff, mean_rgb_diff = get_static_difference(frames)
 
         # getting optical flow
-        avg_velocity = get_optical_flow(frames)
-        
+        max_velocity, mean_velocity = get_optical_flow(frames)
+
         # Getting image context similarity
-        frame_mse, frame_cos_sim = get_image_to_embedding(frames, model, device)
-        
+        frame_mse, frame_cos_sim = get_image_to_embedding(
+            frames, model, device)
+
+        # No. frames
+        no_frames = len(frames)
+
         # Storing the results
-        if scene_id in res:
-            res[scene_id]['rgb_diff'].append(rgb_diff)
-            res[scene_id]['mse'].append(frame_mse)
-            res[scene_id]['cos_sim'].append(frame_cos_sim)
-            res[scene_id]['avg_velocity'].append(avg_velocity)
-        else:
-            res[scene_id] = {'rgb_diff': [rgb_diff], 
-                             'mse': [frame_mse], 
-                             'cos_sim': [frame_cos_sim], 
-                             'avg_velocity': [avg_velocity]}
-        
+        res[scene_id] = {
+            'max_rgb_diff': float(max_rgb_diff),
+            'mean_rgb_diff': float(mean_rgb_diff),
+            'max_velocity': float(max_velocity),
+            'mean_velocity': float(mean_velocity),
+            'mse': float(frame_mse),
+            'cos_sim': float(frame_cos_sim),
+            'no_frames': float(no_frames),
+            'info': data
+        }
+
         print(f'Processing time for scene id {scene_id}: {time.time() - starttime}')
-    
-    # Taking the average result for each clip
-    for scene_id in res:
-        res[scene_id]['rgb_diff'] = float(np.mean(res[scene_id]['rgb_diff']))
-        res[scene_id]['mse'] = float(np.mean(res[scene_id]['mse']))
-        res[scene_id]['cos_sim'] = float(np.mean(res[scene_id]['cos_sim']))
-        res[scene_id]['avg_velocity'] = float(np.mean(res[scene_id]['avg_velocity']))
-    
+
     return res
 
 
 # returning the inference result in the form of
 # {'clip_id': {'static_diff': static_diff, 'mse': mse, 'cos_sim': cos_sim, 'avg_velocity': avg_velocity}}
 
-N_VIDEOS_PER_BATCH = 10
+N_VIDEOS_PER_BATCH = 1
 N_TOTAL_VIDEOS = 18_750
 N_TOTAL_CLIPS = 1_500_000
+TOTAL_CLIPS_TAKEN = 10_000
+CLIPS_TAKEN_PER_BATCH = max(
+    1, int(N_VIDEOS_PER_BATCH / N_TOTAL_VIDEOS * TOTAL_CLIPS_TAKEN))
+metafile_path = './metafiles/hdvg_0.json'
+filename = 'xgboost_model.pth'
 
 if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f'using device: {device}')
+    print(f'CLIPS_TAKEN_PER_BATCH: {CLIPS_TAKEN_PER_BATCH}')
 
-    metafile_path = './metafiles/hdvg_0.json'
     with open(metafile_path, 'r') as f:
         data = json.load(f)
 
     model = get_model()
     model.to(device)
-    
+
     inference_output_dir = 'frame_score_results'
     if not os.path.exists(inference_output_dir):
         os.makedirs(inference_output_dir)
 
-    for i in range(0, 50, N_VIDEOS_PER_BATCH):
+    classifier_model = pickle.load(open(filename, 'rb'))
+
+    for i in range(100, 101, N_VIDEOS_PER_BATCH):
         j = i + N_VIDEOS_PER_BATCH - 1
         print(f'Processing Video {i}-{j}')
         starttime = time.time()
 
         dataset = VideoDataset(data, i, j)
         res = get_metrics(dataset, model, device)
-        
-        n_clips_taken = int(N_VIDEOS_PER_BATCH / N_TOTAL_VIDEOS * N_TOTAL_CLIPS)
-        filtered_scenes = filter_scenes(res, n_clips_taken) 
-        print("No. Scenes Taken:", len(filtered_scenes))
 
-        print(f'Total time: {time.time() - starttime}')
-        print(filtered_scenes)
-        
+        # save res to json
         with open(os.path.join(inference_output_dir, f'inference_result_{i}-{j}.json'), 'w') as f:
+            json.dump(res, f)
+
+        filtered_scenes = filter_scenes(
+            res, CLIPS_TAKEN_PER_BATCH, classifier_model)
+        print("No. Scenes Taken:", len(filtered_scenes))
+        print(f'Total time: {time.time() - starttime}')
+
+        with open(os.path.join(inference_output_dir, f'filtered_scenes_{i}-{j}.json'), 'w') as f:
             json.dump(filtered_scenes, f)
--- utils/filter_scenes.py
+++ utils/filter_scenes.py
@@ -1,13 +1,42 @@
 import numpy
+import pandas as pd
+import json
+import pickle
 
-# TODO!!!: implement the filtering logic
-def filter_scenes(data, n_taken):
+def filter_scenes(data, n_taken, classifier_model):
     """
     data consist of scene_id keys with dict containing: rgb_diff, avg_velocity, mse, cos_sim
     """
 
-    filtered_data = []
-    for scene_id in data:
-        filtered_data.append(scene_id)
+    # data is dict of dict and we want to convert it to list of dict
+    data = list(data.values())
+    data_info = []
+    for keys in data:
+        data_info.append(keys['info'])
+        keys.pop('info')
 
-    return filtered_data[:n_taken]
+    # convert to dataframe
+    data = pd.DataFrame(data)
+
+    # get the prediction
+    pred = classifier_model.predict_proba(data)[:, 1]
+
+    # sort the data_info based on the prediction values
+    data_info = [x for _, x in sorted(zip(pred, data_info), key=lambda pair: pair[0], reverse=True)]
+
+    # return the top n_taken scene_ids
+    return data_info[:n_taken]
+
+
+# Example usage
+if __name__ == '__main__':
+    # load inference_result_0-0.json
+    with open('frame_score_results/inference_result_100-100.json', 'r') as f:
+        data = json.load(f)
+
+    # load the classifier model
+    classifier_model = pickle.load(open('xgboost_model.pth', 'rb'))
+
+    # filter the scenes
+    filtered_scenes = filter_scenes(data, 2, classifier_model)
+    print(filtered_scenes)
